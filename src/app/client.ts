23fec201f3064616fc697fc37517c4c26512ec58
Your Name <sergeialeks232@gmail.com>
2024-07-14 14:50:38 +0800

    Your commit message here

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,6 +1,6 @@
 import { createThirdwebClient } from 'thirdweb';
 
-const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
+const CLIENT_ID = "418c34704b5872fe408942255ec1e8e9" ;
 
 export const client = createThirdwebClient({
     clientId: CLIENT_ID,
