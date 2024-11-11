5311180cf1916ef07f43b38bdd7f33974425a606
Your Name <sergeialeks232@gmail.com>
2024-11-11 23:56:16 +0800

    public

--- components/Staking.tsx
+++ components/Staking.tsx
@@ -62,13 +62,13 @@ export const Staking = () => {
             <h2 className={styles.Titletxt}>MINT DETAILS</h2>
        
 
-                <p className={styles.Regulartxt}>PHASE: WHITELIST CLAIM </p>
+                <p className={styles.Regulartxt}>PHASE: PUBLIC </p>
 
-          <p className={styles.Regulartxt}>MAX MINT: 2 NFTs
+          <p className={styles.Regulartxt}>MAX MINT: 4 NFTs
           </p>
-          <p className={styles.Regulartxt}>MINT COST: 0 BERA
+          <p className={styles.Regulartxt}>MINT COST: 5 BERA
           </p>
-          <p className={styles.Regulartxt}>DURATION: 1 DAY
+          <p className={styles.Regulartxt}>DURATION: Until Competition
           </p>
                 
                 <hr className={styles.divider} />
